928986f58a871e2f363c672251e0692ade75301f
Venus Lu <venuslu5499@gmail.com>
2020-06-26 12:14:23 +0800

    venus: fixed errors

--- src/app.js
+++ src/app.js
@@ -6,8 +6,33 @@ class IndecisionApp extends React.Component{ //class based components
         this.handleAddOption = this.handleAddOption.bind(this);
         this.handleDeleteOption = this.handleDeleteOption.bind(this);
         this.state = {
-            options: props.options
+            options: []
         }
+    }
+
+    componentDidMount() { 
+        try {
+            const json = localStorage.getItem('options');
+            const options = JSON.parse(json);
+    
+            if(options) {
+                this.setState(() => ({options: options}))
+            }  
+        } catch (e) {
+            // Do nothing at all
+        }
+
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if(prevState.options.length !== this.state.options.length) {
+            const json = JSON.stringify(this.state.options)
+            localStorage.setItem('options', json);
+        }  
+    }
+
+    componentWillUnmount() {
+        console.log('unmount');
     }
 
     handleDeleteOptions(){
@@ -63,10 +88,6 @@ class IndecisionApp extends React.Component{ //class based components
     }
 }
 
-IndecisionApp.defaultProps = {
-    options: []
-}
-
 const Header = (props) => {
     return (
         <div>
@@ -97,6 +118,7 @@ const Options = (props) => {
     return (
         <div>
             <button onClick={props.handleDeleteOptions}>Remove All</button>
+            {props.options.length === 0 && <p>Please add an option to get started!</p>}
             {
                 props.options.map((option) => (
                     <Option 
@@ -141,6 +163,10 @@ class AddOption extends React.Component{
         const error = this.props.handleAddOption(option);
 
         this.setState(() => ({error: error}))
+
+        if(!error) {
+            e.target.elements.opt.value = '';
+        }
     }
 
     render(){
