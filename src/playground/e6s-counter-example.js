1587133387d394d3cace65722c179b35313d0f39
Venus Lu <venuslu5499@gmail.com>
2020-06-26 12:12:30 +0800

    venus: added life cycle methods

--- src/playground/e6s-counter-example.js
+++ src/playground/e6s-counter-example.js
@@ -6,8 +6,24 @@ class Counter extends React.Component {
         this.handleMinusOne = this.handleMinusOne.bind(this);
         this.handleReset = this.handleReset.bind(this);
         this.state = {
-            count: props.count
+            count: 0
         };
+    }
+
+    componentDidMount() {
+        const json = localStorage.getItem('count');
+        const count = JSON.parse(json, 10);
+        
+        if(!isNaN(count)){
+            this.setState(() => ({count: count}))
+        } 
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if(prevState.count !== this.state.count){
+            const json = JSON.stringify(this.state.count);
+            localStorage.setItem('count', json);
+        }
     }
 
     handleAddOne(){
@@ -44,10 +60,6 @@ class Counter extends React.Component {
             </div>
         )
     }
-}
-
-Counter.defaultProps = {
-    count: 0
 }
 
 ReactDOM.render(<Counter />, document.getElementById("app"));
